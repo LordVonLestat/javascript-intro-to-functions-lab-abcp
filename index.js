f475463f7aa2af3e8ed900e08c4cbb8feff1e355
Mike Bakker <equanoxproductions@gmail.com>
2018-01-11 02:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
-    console.log("I can't hear you!")
+  console.log("I can't hear you!")
+    else if (string.toUpperCase() === string)
+    console.log("YES INDEED!")
+    
 }
